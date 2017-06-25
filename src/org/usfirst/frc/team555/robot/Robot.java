c40ed933b671375b58d03f7a1b883780312c8f4c
rich kopelow <rich.kopelow@gmail.com>
2017-06-24 21:39:32 -0400

    Breakout of Stronghold code for cyborg

---
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team555.robot;
+
+import org.usfirst.frc.team555.stronghold.SHRobot;
+
+/**
+ * 
+ * This is the main robot definition class.
+ * 
+ */
+public class Robot extends SHRobot {
+	
+}
